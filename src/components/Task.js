e7f78c95cf1d7ce689913c4c984b3c2140218f87
bouquet1 <bukuleta@gmail.com>
2023-06-15 13:59:09 -0700

    displays task list

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Task() {
+function Task({ id, text, category }) {
+  const [taskList, setTaskList] = useState(text, category);
+  //console.log(taskList);
+
+  function handleDelete() {
+    console.log();
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleDelete}>
+        X
+      </button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Task from "./Task";
+import { v4 as uuidv4 } from "uuid";
 
-function TaskList() {
+function TaskList({ tasks }) {
+  const taskList = tasks.map((task) => <Task key={uuidv4()} text={task.text} category={task.category} />);
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      <h3>Task List</h3>
+      <ul>{taskList}</ul>
     </div>
   );
 }
+
+// function TaskList({ tasks }) {
+//   return (
+//     <div className="tasks">
+//       {/* display a list of tasks using Task component */}
+//       <Task tasks={tasks} />
+//     </div>
+//   );
+// }
 
 export default TaskList;
