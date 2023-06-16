5b2732326aa24f7b2882e55e05ae75ac7067dee9
bouquet1 <bukuleta@gmail.com>
2023-06-15 18:17:12 -0700

    CategoryFilter is done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -9,10 +9,16 @@ import { CATEGORIES, TASKS } from "../data";
 // console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        setselectedCategory={setSelectedCategory}
+      />
       <NewTaskForm />
       <TaskList tasks={TASKS} />
     </div>
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,33 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectedCategory, setSelectedCategory }) {
+  function handleClick(category) {
+    setSelectedCategory(category);
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {/* render <button> elements for each category here. The buttons are generated using the map method on the categories array. */}
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={selectedCategory === category ? "selected" : null}
+          onClick={() => handleClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
 
 export default CategoryFilter;
+
+/**
+handleButtonClick takes a category parameter and sets the selected category using the setselectedCategory function.
+The buttons are generated using the map method on the categories array. Each category in the array is mapped to a button element.
+The key prop is set to the category value to help React efficiently update the DOM when the array changes.
+The className prop of each button is conditionally set. If the setselectedCategory value matches the current category, the button will have the "selected" class; otherwise, it will have no additional class.
+The onClick event handler is set to call the handleButtonClick function with the corresponding category as an argument when a button is clicked.
+The button text is set to the current category value. */
